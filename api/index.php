695fc04e052c2f846297ca40891bb7c630acd6d5
Mohammadreza Molaei <mr.mohammad305@gmail.com>
2024-06-01 21:05:37 +0330

    update api

--- api/index.php
+++ api/index.php
@@ -10,8 +10,13 @@ INNER JOIN category
 ON posts.category_id=category.id
 INNER JOIN users
 ON posts.user_id=users.id
-WHERE posts.status = 'publish'")->fetchAll();
+WHERE posts.status = 'publish' LIMIT $limit")->fetchAll();
 
-echo json_encode([
-    'posts' => $posts
-]);
+include '../jdf.php';
+include '../functions.php';
+
+for ($i = 0; $i < count($posts); $i++) {
+    $posts[$i]['6'] = convert_date($posts[$i]['6'], true);
+}
+
+echo json_encode(['posts' => $posts]);
